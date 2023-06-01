add8ee2c33da169c91d770a430a651594ea5ae74
Felipe Araujo <felipe.araujo@capgemini.com>
2023-06-01 09:30:30 -0300

    user delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -43,4 +43,11 @@ public class UserResource {
 
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
 }
